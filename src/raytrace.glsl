8b4fbe900509b525cc7a8fb635454994018de86c
Adnan F. Zafar <adnan.f.zafar@gmail.com>
2022-01-11 12:05:21 -0600

    Updated raytrace GLSL shader with simple Blinn-Phong shading

--- src/raytrace.glsl
+++ src/raytrace.glsl
@@ -12,24 +12,36 @@ layout(std430, binding = 1) buffer primitiveBuffer
 };
 
 struct material_s {
-   float ka;	// ambient reflection coefficient
-	float kd;	// diffuse coefficient
-	float n;	// Phong specular reflection exponent
-	float ks;	// material specular reflection coefficient
+    float ka;	// ambient reflection coefficient
+    float kd;	// diffuse coefficient
+    float n;	// Phong specular reflection exponent
+    float ks;	// material specular reflection coefficient
 };
 
 struct sphere_s {
-	vec4 origin;
-	vec4 diffuse_color;
-	vec4 specular_color;
-	material_s material;
-	float radius;
-	float padding[3]; // structs passed to the GPU via buffer objects need to be multiples of vec4 (128 bytes) so pad the sphere
+    vec4 origin;
+    vec4 diffuse_color;
+    vec4 specular_color;
+    material_s material;
+    float radius;
+    float padding[3]; // structs passed to the GPU via buffer objects need to be multiples of vec4 (128 bytes) so pad the sphere
+};
+
+struct light_s {
+    vec4 origin;
+    vec4 color;
+    int type;
+    float padding[3]; // structs passed to the GPU via buffer objects need to be multiples of vec4 (128 bytes) so pad the light
 };
 
 layout(std430, binding = 2) buffer sphereBuffer
 {
    sphere_s spheres[];
+};
+
+layout(std430, binding = 3) buffer lightsBuffer
+{
+   light_s lights[];
 };
 
 layout(std430, binding = 4) buffer primaryRayBuffer
@@ -48,18 +60,78 @@ void main()
     ivec2 pixel_coords = ivec2(gl_GlobalInvocationID.xy);
 
     // trace against each sphere
-	vec4 ray_origin = vec4(0.0, 0.0, 0.0, 1.0);
+    vec4 ray_origin = vec4(0.0, 0.0, 0.0, 1.0);
     uint index = pixel_coords.y * gl_NumWorkGroups.x * gl_WorkGroupSize.x + pixel_coords.x;
     for(int i = 0; i < spheres.length(); i++) {
-	    vec4 omc = ray_origin - spheres[i].origin;
+        vec4 omc = ray_origin - spheres[i].origin;
         float b = dot(primaryRays[index], omc);
-        float c = dot(omc, omc) - spheres[i].radius * spheres[i].radius;
+        float rsquared = spheres[i].radius * spheres[i].radius;
+        float c = dot(omc, omc) - rsquared;
         float bsqmc = b * b - c;
-		// hit one or the other side of the sphere
-		if (bsqmc >= 0.0) {
-			pixel = vec4(0.4, 0.4, 1.0, 1.0);
-		}
-	}
+        // hit one or the other side of the sphere
+        if (bsqmc >= 0.0) {
+            //pixel = vec4(0.4, 0.4, 1.0, 1.0);
+            //pixel += spheres[i].diffuse_color;
+            
+            
+            // calculate the ray-sphere intersection point
+            
+            vec4 s = spheres[i].origin - ray_origin;
+            float tca = dot(s.xyz, primaryRays[index].xyz);
+            float l2oc = dot(s.xyz,s.xyz);
+            
+            // if the ray originates outside of the sphere and points away, it doesn't intersect
+            if((l2oc > rsquared) && (tca < 0)) {
+                continue;
+            }
+            
+            // determine the half cord distance squared. if this distance is less than 0, that indicates that the ray missed the sphere
+	        float t2hc = rsquared - l2oc + tca * tca;
+	        if(t2hc < 0) {
+		        continue;
+            }
+
+            // FIXME: all of the vector normalization and dot products should be on the first 3 elements
+            
+            // find the intersection point
+            float t = 0.0f;
+
+            // if the ray originated within the sphere, t = tca +sqrt(t2hc)
+            if (l2oc < rsquared) {
+                t = tca + sqrt(t2hc);
+            }
+            // if the ray originated outside the sphere, t = tca - sqrt(t2hc)
+            else if(l2oc > rsquared) {
+                t = tca - sqrt(t2hc);
+            }
+            
+            vec4 hit_point = ray_origin + primaryRays[index] * t;
+            hit_point.w = 1.0f;
+            vec4 v = normalize(ray_origin - hit_point);
+
+            /*
+            for(int j = 0; j < lights.length(); j++) {
+                pixel += lights[j].color * spheres[i].diffuse_color;
+            }
+            */
+
+            // Phong-Blinn shading
+            
+            // for each light source, contribute the color contribution
+            for(int j = 0; j < lights.length(); j++) {
+                
+                vec4 l = normalize(lights[j].origin - hit_point);
+                vec4 n = normalize(hit_point - spheres[i].origin);
+                vec4 h = (v+l)/length(v+l);
+
+                pixel += lights[j].color * (spheres[i].diffuse_color * spheres[i].material.kd * max(0, dot(n.xyz, l.xyz)) + spheres[i].material.ks * max(0, pow(dot(n.xyz, h.xyz), spheres[i].material.n)));
+
+                //pixel = lights[j].color * dot(n.xyz, l.xyz);
+                
+            }
+            
+        }
+    }
 
     // test shader contents here by varying color based on the x,y coordinate
     //pixel.r = gl_GlobalInvocationID.x/(1.0f * gl_NumWorkGroups.x * gl_WorkGroupSize.x);
